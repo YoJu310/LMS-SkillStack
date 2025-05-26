3bdb5920d43610eac051a88105949da0353581df
YoJu310 <singhyojna882@gmail.com>
2025-05-27 00:28:10 +0530

    remove footer nav

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
-import { useNavigate } from 'react-router-dom';
-const navigate = useNavigate();
+// import { useNavigate } from 'react-router-dom';
+// const navigate = useNavigate();
 
 
 const Footer = () => {
